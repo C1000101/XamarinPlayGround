717ca59e30595c3ba69157b170314f7b33bc5e14
C1000101 <alihamood96@gmail.com>
2024-05-27 11:03:16 -0700

    Created a contact list with names, status, and profile images. Added a Models file to store the Contacts class. Implemented the name list using XAML.

--- HelloWorld/HelloWorld/BasicList.xaml.cs
+++ HelloWorld/HelloWorld/BasicList.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelloWorld.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,14 +17,11 @@ namespace HelloWorld
 		{
 			InitializeComponent();
 
-			var names = new List<string>()
-			{
-				"Mosh", 
-				"John",
-				"Bob"
+			listView.ItemsSource = new List<Contact>
+			{ 
+				new Contact { Name = "Smith", ImageUrl = "https://picsum.photos/id/1/1920/1080/" },
+				new Contact { Name = "John", ImageUrl = "https://picsum.photos/id/2/1920/1080/", Status = "Hey, lets talk!"},
 			};
-
-			listView.ItemsSource = names;
 		}
 	}
 }
