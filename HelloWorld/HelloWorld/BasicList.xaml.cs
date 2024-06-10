2ba194624de9b0386ea2402ced60d6932015c350
C1000101 <alihamood96@gmail.com>
2024-06-10 16:16:36 -0700

    Reverts the list back to a basic list with no buttons or images. Adds context actions with a call and delete button.

--- HelloWorld/HelloWorld/BasicList.xaml.cs
+++ HelloWorld/HelloWorld/BasicList.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using HelloWorld.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,22 +14,38 @@ namespace HelloWorld
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class BasicList : ContentPage
 	{
+		private ObservableCollection<Contact> _contacts;
+
 		public BasicList()
 		{
 			InitializeComponent();
 
-			listView.ItemsSource = new List<ContactGroup>
+			_contacts = new ObservableCollection<Contact>
 			{
-				new ContactGroup("A", "A")
-				{
-					new Contact { Name = "Ali", ImageUrl = "https://picsum.photos/id/1/1920/1080/" },
-				},
-
-				new ContactGroup("B", "B")
-				{				
-					new Contact { Name = "Bob", ImageUrl = "https://picsum.photos/id/2/1920/1080/", Status = "Hey, lets talk!"},
-				},
+				new Contact { Name = "Ali", ImageUrl = "https://picsum.photos/id/1/1920/1080/" },
+				new Contact { Name = "Bob", ImageUrl = "https://picsum.photos/id/2/1920/1080/", Status = "Hey, lets talk!"},
 			};
+
+			listView.ItemsSource = _contacts;
+
+			//listView.ItemsSource = new List<Contact>
+			//{
+			//	new Contact { Name = "Ali", ImageUrl = "https://picsum.photos/id/1/1920/1080/" },	
+			//	new Contact { Name = "Bob", ImageUrl = "https://picsum.photos/id/2/1920/1080/", Status = "Hey, lets talk!"},
+			//};
+
+			//listView.ItemsSource = new List<ContactGroup>
+			//{
+			//	new ContactGroup("A", "A")
+			//	{
+			//		new Contact { Name = "Ali", ImageUrl = "https://picsum.photos/id/1/1920/1080/" },
+			//	},
+
+			//	new ContactGroup("B", "B")
+			//	{
+			//		new Contact { Name = "Bob", ImageUrl = "https://picsum.photos/id/2/1920/1080/", Status = "Hey, lets talk!"},
+			//	},
+			//};
 		}
 
 		private void listView_ItemTapped(object sender, ItemTappedEventArgs e)
@@ -41,6 +58,20 @@ namespace HelloWorld
 		{
 			var contact = e.SelectedItem as Contact;
 			DisplayAlert("Selected", contact.Name, "OK");
+		}
+
+		private void Call_Clicked(object sender, EventArgs e)
+		{
+			var menuItem = sender as MenuItem;
+			var contact = menuItem.CommandParameter as Contact;
+
+			DisplayAlert("Cell", contact.Name, "OK");
+		}
+
+		private void Delete_Clicked(object sender, EventArgs e)
+		{
+			var contact = (sender as MenuItem).CommandParameter as Contact;
+			_contacts.Remove(contact);
 		}
 	}
 }
