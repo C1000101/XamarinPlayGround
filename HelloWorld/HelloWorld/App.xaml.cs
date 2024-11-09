6c85e40f48f06b93c20a593a23438308724ab8ce
C1000101 <alihamood96@gmail.com>
2024-11-08 16:30:33 -0800

    Xamarin__forms and setting pages__Table View: Added a content page for tabel view. Tried to make a page next and back featue but it didn't work, so it is adds, but comments it out.

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
 		{
 			InitializeComponent();
 
-			MainPage = new UserControlsTwo();
+			MainPage = new TableViewUserControls();
 			//MainPage = new NavigationPage(new ContactsPage());
 			//MainPage = new NavigationPage(new WelcomePage())
 			//{
--- HelloWorld/HelloWorld/UserControlsTwo.xaml.cs
+++ HelloWorld/HelloWorld/UserControlsTwo.xaml.cs
@@ -12,14 +12,47 @@ namespace HelloWorld
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class UserControlsTwo : ContentPage
 	{
-		public UserControlsTwo()
+		Page mainPage { get; set; }	
+		List<ContentPage> pages;
+		int pageIndex = 0;
+
+		public UserControlsTwo(Page _mainPage)
 		{
 			InitializeComponent();
+			this.mainPage = _mainPage;
+			pages = new List<ContentPage>();
+			pages.Add(this);
+			pages.Add(new TableViewUserControls());
 		}
 
 		private void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
 		{
 
         }
-    }
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+
+		}
+
+		//private void ButtonNext_Cliecked(object sender, EventArgs e)
+		//{
+		//	if (pageIndex == 0)
+		//	{
+		//		pageIndex = 1;
+		//		mainPage = pages[pageIndex];
+		//		mainPage.ForceLayout();
+		//	}
+		//}
+
+		//private void ButtonBack_Clicked(object sender, EventArgs e)
+		//{
+		//	if (pageIndex == 1)
+		//	{
+		//		pageIndex = 0;
+		//		mainPage = pages[pageIndex];
+		//		mainPage.ForceLayout();
+		//	}
+		//}
+	}
 }
