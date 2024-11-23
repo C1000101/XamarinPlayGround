efa39733f728a97346b5c5144ffe9eea1bdef2ea
C1000101 <alihamood96@gmail.com>
2024-11-23 09:05:37 -0800

    Data Access: cleaner implementation for application properties using data binding

---
+++ HelloWorld/HelloWorld.Android/Presistence/SQLiteDb.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HelloWorld.Droid.Presistence;
+using HelloWorld.Persistence;
+using SQLite;
+using System.IO;
+
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+
+namespace HelloWorld.Droid.Presistence
+{
+	public class SQLiteDb : ISQLiteDb
+	{
+		public SQLiteAsyncConnection GetConnection()
+		{
+			var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyComputer);
+			var path = Path.Combine(documentPath, "MySQLite.db3");
+			return new SQLiteAsyncConnection(path);
+		}
+
+	}
+}
